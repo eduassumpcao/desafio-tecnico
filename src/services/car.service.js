cbb3ae533cd2bab32808a6797f34cca5fb0fce41
eduassumpcao <151387203+eduassumpcao@users.noreply.github.com>
2023-11-18 18:51:23 -0300

    refactor car service

--- src/services/car.service.js
+++ src/services/car.service.js
@@ -1,105 +1,101 @@
-const BadRequest = require('../errors/BadRequest')
-const DuplicateError = require('../errors/DuplicateError')
-const NotFound = require('../errors/NotFound')
-const helper = require('../utils/helper.util')
+const BadRequest = require('../errors/BadRequest');
+const DuplicateError = require('../errors/DuplicateError');
+const NotFound = require('../errors/NotFound');
+const helper = require('../utils/helper.util');
 
-let cars = []
+let cars = [];
 
-const getCars = () => {
-  return cars
-}
+const getCars = () => cars;
 
 const getCar = (plate) => {
-  const upperCasePlate = plate.toUpperCase()
-  const existingCar = cars.find((car) => car.plate === upperCasePlate)
+  const upperCasePlate = plate.toUpperCase();
+  const existingCar = cars.find((car) => car.plate === upperCasePlate);
 
   if (!existingCar) {
-    throw new NotFound('Car with this plate not found.')
+    throw new NotFound(`Car with plate '${upperCasePlate}' not found.`);
   }
 
-  return existingCar
-}
+  return existingCar;
+};
 
 const registerCar = (newCar) => {
-  const { plate, color, brand } = newCar
+  const { plate, color, brand } = newCar;
 
   if (!plate || !color || !brand) {
     throw new BadRequest(
       'Invalid request. Please provide a valid JSON object with properties: plate, color, brand.'
-    )
+    );
   }
-  const upperCasePlate = plate.toUpperCase()
+
+  const upperCasePlate = plate.toUpperCase();
+  const upperCaseColor = color.toUpperCase();
+  const upperCaseBrand = brand.toUpperCase();
 
   if (!helper.validateBrazilianPlate(upperCasePlate)) {
     throw new BadRequest(
       'Invalid Brazilian plate format. It should have three letters followed by four digits.'
-    )
+    );
   }
 
-  const existingCar = cars.find((car) => car.plate === upperCasePlate)
-
-  if (existingCar) {
-    throw new DuplicateError('Car with this plate already exists.')
+  if (cars.some((car) => car.plate === upperCasePlate)) {
+    throw new DuplicateError(`Car with plate '${upperCasePlate}' already exists.`);
   }
 
-  const car = { plate: upperCasePlate, color, brand }
-  cars.push(car)
+  const car = {
+    plate: upperCasePlate,
+    color: upperCaseColor,
+    brand: upperCaseBrand,
+  };
 
-  return car
-}
+  cars.push(car);
+
+  return car;
+};
 
 const updateCar = (originalPlate, newCarData) => {
-  const { plate, color, brand } = newCarData
+  const { plate, color, brand } = newCarData;
 
   if (!plate || !color || !brand) {
     throw new BadRequest(
-      'Invalid request. Please provide a valid JSON object with properties: newPlate, newColor, newBrand.'
-    )
+      'Invalid request. Please provide a valid JSON object with properties: plate, color, brand.'
+    );
   }
 
-  const originalPlateUpperCase = originalPlate.toUpperCase()
-  const newPlateUpperCase = plate.toUpperCase()
+  const originalPlateUpperCase = originalPlate.toUpperCase();
+  const newPlateUpperCase = plate.toUpperCase();
+  const newColorUpperCase = color.toUpperCase();
+  const newBrandUpperCase = brand.toUpperCase();
 
-  if (!helper.validateBrazilianPlate(plate)) {
+  if (!helper.validateBrazilianPlate(newPlateUpperCase)) {
     throw new BadRequest(
       'Invalid Brazilian plate format. It should have three letters followed by four digits.'
-    )
+    );
   }
 
-  // Check if the provided plate exists.
-  const existingCarIndex = cars.findIndex(
-    (car) => car.plate === originalPlateUpperCase
-  )
+  const existingCarIndex = cars.findIndex((car) => car.plate === originalPlateUpperCase);
 
   if (existingCarIndex === -1) {
-    throw new NotFound('Car with this plate not found.')
+    throw new NotFound(`Car with plate '${originalPlateUpperCase}' not found.`);
   }
 
-  // Check if the new plate already exists.
-  if (originalPlateUpperCase !== newPlateUpperCase) {
-    const newPlateExists = cars.some((car) => car.plate === newPlateUpperCase)
-
-    if (newPlateExists) {
-      throw new DuplicateError('Car with this plate already exists.')
-    }
+  if (originalPlateUpperCase !== newPlateUpperCase && cars.some((car) => car.plate === newPlateUpperCase)) {
+    throw new DuplicateError(`Car with plate '${newPlateUpperCase}' already exists.`);
   }
 
-  // Update the car with the new information.
   const updatedCar = {
     plate: newPlateUpperCase,
-    color: color,
-    brand: brand,
-  }
+    color: newColorUpperCase,
+    brand: newBrandUpperCase,
+  };
 
-  // Update the car in the array.
-  cars[existingCarIndex] = updatedCar
+  cars[existingCarIndex] = updatedCar;
 
-  return updatedCar
-}
+  return updatedCar;
+};
 
 module.exports = {
   registerCar,
   getCars,
   getCar,
   updateCar,
-}
+};
