22899bbde275e74959991166ef9c8f87f12e1bff
eduassumpcao <151387203+eduassumpcao@users.noreply.github.com>
2023-11-20 10:15:42 -0300

    adds a helper function to validate the timestamp

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -4,6 +4,21 @@ const validateBrazilianPlate = (plateToValidate) => {
   return brazilianPlateRegex.test(plateToValidate)
 }
 
+const validateTimestamp = (timestamp) => {
+  if (typeof timestamp !== 'string' || !/^\d+$/.test(timestamp)) return false
+
+  timestamp = parseInt(timestamp, 10)
+  const now = Date.now()
+  const limit = now - 100 * 365 * 24 * 60 * 60 * 1000
+
+  if (timestamp < limit || timestamp > now) {
+    return false
+  }
+
+  return true
+}
+
 module.exports = {
   validateBrazilianPlate,
+  validateTimestamp,
 }
